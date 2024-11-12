eb9b7db94e1173a48d5f43d05b3fd6c273b2b56c
Wangwei-cheng <tyjh22401@gmail.com>
2024-11-12 19:05:33 +0800

    save and load progressing

---
+++ Assets/DataPersistence/Data/GameData.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameData
+{
+    public float zombieWalkingSpeed;
+
+    public GameData()
+    {
+        this.zombieWalkingSpeed = 100;
+    }
+}
--- Assets/Scripts/Zombie.cs
+++ Assets/Scripts/Zombie.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Zombie : MonoBehaviour
+public class Zombie : MonoBehaviour, IDataPersistence
 {
-    public Animator zombie_anima;
-    public Rigidbody zombie_rigid;
-    public Transform zombie_trans;
-    public float walk_speed = 50;
+    public Animator zombieAnima;
+    public Rigidbody zombieRigid;
+    public Transform zombieTrans;
+    public float walkingSpeed = 50;
 
     public ZombieState[] states;
     public int curState = 0;
@@ -16,6 +16,16 @@ public class Zombie : MonoBehaviour
     private float stopTime;
     private float curTime;
     private int totalStates;
+
+    public void LoadData(GameData data)
+    {
+        this.walkingSpeed = data.zombieWalkingSpeed;
+    }
+
+    public void SaveData(ref GameData data)
+    {
+        data.zombieWalkingSpeed = this.walkingSpeed;
+    }
 
     private void Start()
     {
@@ -27,7 +37,7 @@ public class Zombie : MonoBehaviour
     {
         if (walking)
         {
-            zombie_rigid.velocity = transform.forward * walk_speed * Time.deltaTime;
+            zombieRigid.velocity = transform.forward * walkingSpeed * Time.deltaTime;
         }
     }
 
@@ -39,24 +49,29 @@ public class Zombie : MonoBehaviour
         {
             if (curTime - stopTime >= 2)
             {
-                zombie_trans.Rotate(0, states[curState].rotatingDegree, 0);
+                zombieTrans.Rotate(0, states[curState].rotatingDegree, 0);
                 walking = true;
-                zombie_anima.SetTrigger("Run");
-                zombie_anima.ResetTrigger("Idle");
+                zombieAnima.SetTrigger("Run");
+                zombieAnima.ResetTrigger("Idle");
             }
         }
         if (walking)
         {
-            if (zombie_trans.position.x * states[curState].xDirection >= states[curState].goal.x * states[curState].xDirection &&
-                zombie_trans.position.z * states[curState].zDirection >= states[curState].goal.z * states[curState].zDirection)
+            if (zombieTrans.position.x * states[curState].xDirection >= states[curState].goal.x * states[curState].xDirection &&
+                zombieTrans.position.z * states[curState].zDirection >= states[curState].goal.z * states[curState].zDirection)
             {
                 walking = false;
-                zombie_trans.position = new Vector3(states[curState].goal.x, transform.position.y, states[curState].goal.z);
+                zombieTrans.position = new Vector3(states[curState].goal.x, transform.position.y, states[curState].goal.z);
                 stopTime = Time.time;
                 curState = (curState + 1)%totalStates;
-                zombie_anima.ResetTrigger("Run");
-                zombie_anima.SetTrigger("Idle");
+                zombieAnima.ResetTrigger("Run");
+                zombieAnima.SetTrigger("Idle");
             }
         }
+    }
+
+    public void SetWalkingSpeed(float speed)
+    {
+        walkingSpeed = speed;
     }
 }
